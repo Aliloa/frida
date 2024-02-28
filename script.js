e0affb4c5f8923ecc44f8ff1002d4815cb166108
HugoLcrf <lecerf.hugo04@gmail.com>
2024-02-28 18:10:13 +0100

    Ajoute button mode nuit fonctionnel

--- script.js
+++ script.js
@@ -9,3 +9,29 @@ links.forEach((link) => {
     nav.classList.remove("active");
   });
 });
+
+if(!localStorage.getItem("theme")){
+  localStorage.setItem("theme","nuit")
+}else{
+  if(localStorage.getItem("theme") === "light"){
+      document.body.classList.add("nuit");
+  }else{
+      localStorage.setItem("theme","nuit")
+  }
+}
+
+try{
+  document.getElementById("themebutton").addEventListener("click", (e) => {
+      if(localStorage.getItem("theme") === "nuit"){
+
+          localStorage.setItem("theme","light")
+          document.body.classList.add("nuit");
+
+      }else{
+
+          localStorage.setItem("theme","nuit")
+          document.body.classList.remove("nuit");
+
+      }
+  });
+}catch(error){};
