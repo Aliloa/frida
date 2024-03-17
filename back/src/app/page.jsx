2de7e2569e92d20d22f36575d0b3affa05f26eff
Alina <alinaspicot@gmail.com>
2024-03-17 17:10:34 +0100

    Tout le back avec React
    Formulaire marche plus

---
+++ back/src/app/page.jsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { Tableau } from "@/components/affichage/Tableau";
+import { Formulaire } from "@/components/insertion/Formulaire";
+import { Nav } from "@/components/nav/Nav";
+import React from "react";
+
+
+const Home = () => {
+  
+  return (
+      <section>
+      <h1>TABLEAU</h1>
+      <Tableau />
+    </section>
+  )
+
+};
+
+
+export default Home;
---
+++ back/src/components/supprimer/Delete.jsx
@@ -0,0 +1,21 @@
+export const Delete = ({ id }) => {
+    const API = `http://localhost/frida/api/reservations.php` + '?id=' + id;
+    
+    const supprimerReservation = (e) => {
+            
+        fetch(API, {
+            method: 'DELETE',
+        }).then((response) => {
+            if (!response.ok) {
+                throw new Error('Erreur lors de la requête.');
+            }
+        }).catch(error => {
+            console.error('Erreur lors de la requête:', error);
+        });
+        window.location.reload();
+    }    
+
+    return <button onClick={supprimerReservation}>Delete</button>;
+}
+
+export default Delete;
